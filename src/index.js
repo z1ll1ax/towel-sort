8cee023713a2e00a87d15e745ba749b9b3ff0a4c
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2024-08-15 16:07:29 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  let new_arr = []
+  for (let i = 0; i < matrix.length; i++){
+    if (i % 2 !== 0) matrix[i] = matrix[i].reverse();
+    matrix[i].forEach(element => {
+      new_arr[new_arr.length] = element;
+    });
+  }
+  return new_arr;
 }
